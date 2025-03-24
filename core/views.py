4add4e0170f0c3f3639fea340f32cc1cc7deeee0
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-24 22:26:54 +0300

    tamplates: шаблон страницы спасибо и view, css, шрифт Playfair Display

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
 MENU = [
     {'title': 'Главная', 'url': '/', 'active': True},
@@ -9,3 +9,11 @@ MENU = [
     # {'title': 'Оставить отзыв', 'url': '/review/create/', 'active': True},
     {'title': 'Запись на прием', 'url': '#orderForm', 'active': True},
 ]
+
+class ThanksView(TemplateView):
+    template_name = 'thanks.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = MENU
+        return context
