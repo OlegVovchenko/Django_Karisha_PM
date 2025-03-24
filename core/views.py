1c4b3aab9953876eeec333d7780535d87c1ddc3a
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-24 21:19:55 +0300

    menu: константа меню

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+MENU = [
+    {'title': 'Главная', 'url': '/', 'active': True},
+    {'title': 'Мастера', 'url': '#masters', 'active': True},
+    {'title': 'Услуги', 'url': '#services', 'active': True},
+    # {'title': 'Портфолио', 'url': '#portfolio', 'active': True},
+    # {'title': 'Отзывы', 'url': '#reviews', 'active': True},
+    # {'title': 'Оставить отзыв', 'url': '/review/create/', 'active': True},
+    {'title': 'Запись на прием', 'url': '#orderForm', 'active': True},
+]
