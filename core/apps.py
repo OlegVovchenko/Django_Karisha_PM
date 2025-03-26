e0ebf29488b3743fdca4c42d9cf4cd0e4a2f8b34
Oleg Vovchenko <o.vovchenko91@gmail.com>
2025-03-26 21:27:38 +0300

    feat: обработчик сигналов для автоматической проверки и изменения статуса отзывов

--- core/apps.py
+++ core/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = 'Салон красоты "Karisha PM"'
+
+    def ready(self):
+        import core.signals
